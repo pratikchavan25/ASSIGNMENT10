80391cce93019d0c3d8f83db7256d8e2814e066f
Pratik <Pratik@DESKTOP-9189TTI>
2023-06-09 14:25:34 +0530

    Add project files.

---
+++ ASSIGNMENT10/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 10. Write a program to check whether a year is leap year or not ?
+
+using System.ComponentModel.Design;
+
+int year;
+
+Console.WriteLine("enter a year");
+year = Convert.ToInt32(Console.ReadLine());
+
+if (year % 4 == 0) 
+{
+    Console.WriteLine("it is a leap year");
+}
+else
+{
+    Console.WriteLine("it is not a leap year");
+}
